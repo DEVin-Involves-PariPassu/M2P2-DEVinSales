9aae10bdf3aae3c5efa245b2d1f9c231f0ebfeb3
Lucas Bueno Cesario <lbc92@hotmail.com>
2022-03-01 14:34:30 -0300

    Cria a tabela Estado

--- src/main/java/br/com/senai/p2m02/devinsales/model/configuration/SpringSecurityConfiguration.java
+++ src/main/java/br/com/senai/p2m02/devinsales/model/configuration/SpringSecurityConfiguration.java
@@ -1,4 +1,4 @@
-package br.com.senai.p2m02.devinsales.configuration;
+package br.com.senai.p2m02.devinsales.model.configuration;
 
 import br.com.senai.p2m02.devinsales.interceptor.SecurityInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
