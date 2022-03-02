d8d258beefc46ad36f0aacb502d8f296c8a0cb6b
Victor Ferrando <victor.ferrando@hotmail.com>
2022-03-02 20:58:19 -0300

    Add entity Sale

---
+++ tabela_venda.sql
@@ -0,0 +1,9 @@
+create table venda(
+                      id bigserial primary key,
+                      id_comprador bigint not null,
+                      id_vendedor bigint,
+                      dt_venda datetime,
+
+                      constraint fk_vd_comprador foreign key (id_comprador) references usuario(id),
+                      constraint fk_vd_vendedor foreign key (id_vendedor) references usuario(id)
+w
