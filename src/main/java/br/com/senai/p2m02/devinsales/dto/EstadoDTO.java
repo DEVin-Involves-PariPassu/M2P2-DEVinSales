2ca0ea02db1ca3f753b62502e79da4f1e668e611
Evandro Dias <evandro10dias@gmail.com>
2022-03-02 20:51:16 -0300

    Cria dto de endereco e getters e setters de todos DTOs

--- src/main/java/br/com/senai/p2m02/devinsales/dto/EstadoDTO.java
+++ src/main/java/br/com/senai/p2m02/devinsales/dto/EstadoDTO.java
@@ -10,4 +10,28 @@ public class EstadoDTO {
 
     @NotNull(message = "Sigla é requerido")
     private String sigla;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
 }
