7a0d371928090351a92ea15bdd76d87ac3d7b233
Victor Ferrando <victor.ferrando@hotmail.com>
2022-03-02 21:21:31 -0300

    Syntax correction on line 9 from 'w' to ')'

--- tabela_venda.sql
+++ tabela_venda.sql
@@ -6,4 +6,4 @@ create table venda(
 
                       constraint fk_vd_comprador foreign key (id_comprador) references usuario(id),
                       constraint fk_vd_vendedor foreign key (id_vendedor) references usuario(id)
-w
+)
