767114cc3e56e690946cb44d4a902dfcfa6dcb71
felixcolombo <89814084+felixcolombo@users.noreply.github.com>
2022-03-03 22:31:52 -0300

    Cria EnderecoEntityService

--- src/main/java/br/com/senai/p2m02/devinsales/repository/CidadeEntityRepository.java
+++ src/main/java/br/com/senai/p2m02/devinsales/repository/CidadeEntityRepository.java
@@ -1,9 +1,12 @@
 package br.com.senai.p2m02.devinsales.repository;
 
 import br.com.senai.p2m02.devinsales.model.CidadeEntity;
+import br.com.senai.p2m02.devinsales.model.EstadoEntity;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CidadeEntityRepository extends CrudRepository <CidadeEntity, Long>{
+public interface CidadeEntityRepository extends CrudRepository <CidadeEntity, Long>,
+        JpaSpecificationExecutor<CidadeEntity> {
 }
