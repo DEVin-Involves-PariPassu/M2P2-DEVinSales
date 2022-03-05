1bd1976dcb393b1876f5a2982fd9a522c792e700
Daiana Michels <daianaem.12@gmail.com>
2022-03-05 10:01:16 -0300

    feat: Altera o nome da classe Controller

--- src/main/java/br/com/senai/p2m02/devinsales/api/v1/ProductController.java
+++ src/main/java/br/com/senai/p2m02/devinsales/api/v1/ProductController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/product")
-public class ProductEntityController {
+public class ProductController {
 
     @Autowired
     ProductEntityRepository repository;
