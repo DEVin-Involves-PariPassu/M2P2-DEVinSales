092779b07c69223fa1e75b6646f295de7701092f
Daiana Michels <daianaem.12@gmail.com>
2022-03-05 11:58:45 -0300

    feat: Adiciona atributos e métodos a classe ProductPostParameter

--- src/main/java/br/com/senai/p2m02/devinsales/parameter/ProductPostParameter.java
+++ src/main/java/br/com/senai/p2m02/devinsales/parameter/ProductPostParameter.java
@@ -1,4 +1,33 @@
 package br.com.senai.p2m02.devinsales.parameter;
 
+import java.math.BigDecimal;
+
 public class ProductPostParameter {
+
+    private String nome;
+    private BigDecimal preco_sugerido;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getPreco_sugerido() {
+        return preco_sugerido;
+    }
+
+    public void setPreco_sugerido(BigDecimal preco_sugerido) {
+        this.preco_sugerido = preco_sugerido;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductPostParameter{" +
+                "nome='" + nome + '\'' +
+                ", preco_sugerido=" + preco_sugerido +
+                '}';
+    }
 }
