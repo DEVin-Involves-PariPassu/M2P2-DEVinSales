7306ebff6daf966049f6e0425fde8bb520500525
Daiana Michels <daianaem.12@gmail.com>
2022-03-04 22:02:37 -0300

    feat: Cria a classe ProductEntityController

---
+++ src/main/java/br/com/senai/p2m02/devinsales/api/v1/ProductEntityController.java
@@ -0,0 +1,11 @@
+package br.com.senai.p2m02.devinsales.api.v1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductEntityController {
+
+
+}
