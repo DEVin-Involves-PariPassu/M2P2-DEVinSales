331368581f67ddc242f442101e848d6d70c2f654
Silvia Teixeira <silvia.teixeira@involves.com>
2022-03-05 16:43:43 -0300

    Created DeliveryRepository

---
+++ src/main/java/br/com/senai/p2m02/devinsales/repository/DeliveryRepository.java
@@ -0,0 +1,12 @@
+package br.com.senai.p2m02.devinsales.repository;
+
+
+import br.com.senai.p2m02.devinsales.model.DeliveryEntity;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.CrudRepository;
+
+
+
+public interface DeliveryRepository extends CrudRepository<DeliveryEntity, Long> ,
+        JpaSpecificationExecutor<DeliveryEntity>{
+};
