90d185b76a53d301641eb26d343a6d9c62d699d8
Daiana Michels <daianaem.12@gmail.com>
2022-03-05 16:12:06 -0300

    feat: Adiciona validações ao @PostMapping

--- src/main/java/br/com/senai/p2m02/devinsales/api/v1/ProductController.java
+++ src/main/java/br/com/senai/p2m02/devinsales/api/v1/ProductController.java
@@ -1,9 +1,16 @@
 package br.com.senai.p2m02.devinsales.api.v1;
 
+import br.com.senai.p2m02.devinsales.dto.ProductDTO;
+import br.com.senai.p2m02.devinsales.model.UserEntity;
 import br.com.senai.p2m02.devinsales.repository.ProductRepository;
+import br.com.senai.p2m02.devinsales.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("/product")
@@ -11,5 +18,32 @@ public class ProductController {
 
     @Autowired
     ProductRepository repository;
+
+    @Autowired
+    ProductService service;
+
+
+    @PostMapping
+    public ResponseEntity <Long> post(@RequestAttribute("loggedUser") UserEntity loggedUser,
+                                      @Valid @RequestBody ProductDTO productDTO) {
+
+            if(!loggedUser.canWrite("produto")){
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+            }
+            if (productDTO.getNome().isBlank() || productDTO.getPreco_sugerido() == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+            if(productDTO.getNome().isBlank() == false){
+                service.isUniqueNomeProduct(productDTO);
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+            if (productDTO.getPreco_sugerido().compareTo(BigDecimal.ZERO) <= 0){
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+
+            Long productId = service.insert(productDTO);
+            return new ResponseEntity<>(productId, HttpStatus.CREATED);
+
+    }
 
 }
