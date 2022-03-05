8d47ce5a9f997b0b01260035a1ed5c0f3042829d
Daiana Michels <daianaem.12@gmail.com>
2022-03-04 21:53:07 -0300

    feat: Cria a classe ProductEntity

---
+++ src/main/java/br/com/senai/p2m02/devinsales/model/ProductEntity.java
@@ -0,0 +1,62 @@
+package br.com.senai.p2m02.devinsales.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity(name="produto")
+public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "productger")
+    @SequenceGenerator(name = "productger", sequenceName = "product_id_seq", allocationSize = 1)
+    private long id;
+
+    private String nome;
+    private BigDecimal preco_sugerido;
+
+    public ProductEntity(long id, String nome, BigDecimal preco_sugerido) {
+        this.id = id;
+        this.nome = nome;
+        this.preco_sugerido = preco_sugerido;
+    }
+
+    public ProductEntity() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getPreco_sugerido() {
+        return preco_sugerido;
+    }
+
+    public void setPreco_sugerido(BigDecimal preco_sugerido) {
+        this.preco_sugerido = preco_sugerido;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductEntity{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", preco_sugerido=" + preco_sugerido +
+                '}';
+    }
+
+
+}
