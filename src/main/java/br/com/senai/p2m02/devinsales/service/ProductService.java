17d51ef0544b7e0068d6da1028e6e80310c517dd
Daiana Michels <daianaem.12@gmail.com>
2022-03-05 16:03:59 -0300

    feat: Cria a classe ProductService e adiciona métodos

--- src/main/java/br/com/senai/p2m02/devinsales/service/ProductService.java
+++ src/main/java/br/com/senai/p2m02/devinsales/service/ProductService.java
@@ -1,2 +1,46 @@
-package br.com.senai.p2m02.devinsales.service;public class ProductService {
+package br.com.senai.p2m02.devinsales.service;
+
+import br.com.senai.p2m02.devinsales.dto.ProductDTO;
+import br.com.senai.p2m02.devinsales.model.ProductEntity;
+import br.com.senai.p2m02.devinsales.repository.ProductRepository;
+import jakarta.persistence.EntityExistsException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    ProductRepository productRepository;
+
+
+    //para inserir um novo produto - usado no PostMapping
+    public Long insert(@Valid ProductDTO productDTO) {
+        ProductEntity product = new ProductEntity(productDTO.getId(),
+                productDTO.getNome(),
+                productDTO.getPreco_sugerido());
+        productRepository.produtos.add(product);
+        return product.getId();
+    }
+
+    //busca um produto pelo Id e retorna ele
+    public ProductEntity findById(Long productId) {
+        for (ProductEntity product : productRepository.produtos) {
+            if (product.getId() == productId)
+                return product;
+        }
+        return null;
+    }
+
+    public void isUniqueNomeProduct(ProductDTO productDTO) {
+        Optional<ProductEntity> optionalProduct = productRepository.findByNome(productDTO.getNome());
+        if(optionalProduct.isPresent()){
+            throw new EntityExistsException("Já existe um produto cadastrado com o nome " + productDTO.getNome());
+        }
+    }
+
+
 }
