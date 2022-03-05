aae61bc16485c23190ae872d4f656875c7f87f42
Daiana Michels <daianaem.12@gmail.com>
2022-03-05 09:28:06 -0300

    feat: Adiciona a anotação @Repository na interface ProductEntityRepository

--- src/main/java/br/com/senai/p2m02/devinsales/repository/ProductEntityRepository.java
+++ src/main/java/br/com/senai/p2m02/devinsales/repository/ProductEntityRepository.java
@@ -2,7 +2,9 @@ package br.com.senai.p2m02.devinsales.repository;
 
 import br.com.senai.p2m02.devinsales.model.ProductEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductEntityRepository extends CrudRepository<ProductEntity, Long> {
 
 }
