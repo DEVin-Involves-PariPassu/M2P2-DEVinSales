868f48977fb3611c06d5a371d887a20a15f04bbc
Silvia Teixeira <silvia.teixeira@involves.com>
2022-03-06 15:31:03 -0300

    Merge

--- src/main/java/br/com/senai/p2m02/devinsales/model/ItemVendaEntity.java
+++ src/main/java/br/com/senai/p2m02/devinsales/model/ItemVendaEntity.java
@@ -4,6 +4,7 @@ package br.com.senai.p2m02.devinsales.model;
 import jakarta.persistence.*;
 
 import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
 
 @Entity
 @Table(name="item_venda")
@@ -14,16 +15,18 @@ public class ItemVendaEntity {
     @SequenceGenerator(name = "itemvendager", sequenceName = "item_venda_id_ger", allocationSize = 1)
     private Long id;
 
-    @NotNull
-    //mapear FK para Entidade Venda
-    private Integer idVenda;
+    @OneToOne(cascade = CascadeType.ALL)
+    @MapsId("idVenda")
+    @JoinColumn(name = "id_venda", referencedColumnName = "id")
+    private VendaEntity idVenda;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @MapsId("idProduto")
+    @JoinColumn(name = "id_produto", referencedColumnName = "id")
+    private ProductEntity idProduto;
 
     @NotNull
-    //mapear FK para Entidade Produto
-    private Integer idProduto;
-
-    @NotNull
-    private Integer precoUnitario;
+    private BigDecimal precoUnitario;
 
     @NotNull
     private Integer quantidade;
@@ -36,27 +39,27 @@ public class ItemVendaEntity {
         this.id = id;
     }
 
-    public Integer getIdVenda() {
+    public VendaEntity getIdVenda() {
         return idVenda;
     }
 
-    public void setIdVenda(Integer idVenda) {
+    public void setIdVenda(VendaEntity idVenda) {
         this.idVenda = idVenda;
     }
 
-    public Integer getIdProduto() {
+    public ProductEntity getIdProduto() {
         return idProduto;
     }
 
-    public void setIdProduto(Integer idProduto) {
+    public void setIdProduto(ProductEntity idProduto) {
         this.idProduto = idProduto;
     }
 
-    public Integer getPrecoUnitario() {
+    public BigDecimal getPrecoUnitario() {
         return precoUnitario;
     }
 
-    public void setPrecoUnitario(Integer precoUnitario) {
+    public void setPrecoUnitario(BigDecimal precoUnitario) {
         this.precoUnitario = precoUnitario;
     }
 
