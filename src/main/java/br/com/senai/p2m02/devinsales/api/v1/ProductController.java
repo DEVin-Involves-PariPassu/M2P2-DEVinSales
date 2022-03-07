509b0867873ce73040f2c4cac17ea39bf01e1700
Daiana Michels <daianaem.12@gmail.com>
2022-03-07 13:53:47 -0300

    feat: Simplifica validações do método Post

--- src/main/java/br/com/senai/p2m02/devinsales/api/v1/ProductController.java
+++ src/main/java/br/com/senai/p2m02/devinsales/api/v1/ProductController.java
@@ -9,8 +9,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
 import javax.validation.Valid;
-import java.math.BigDecimal;
+
 
 @RestController
 @RequestMapping("/product")
@@ -22,28 +23,15 @@ public class ProductController {
     @Autowired
     ProductService service;
 
-
     @PostMapping
-    public ResponseEntity <Long> post(@RequestAttribute("loggedUser") UserEntity loggedUser,
-                                      @Valid @RequestBody ProductDTO productDTO) {
-
+    public ResponseEntity<Long> post(@RequestAttribute("loggedUser") UserEntity loggedUser,
+                                              @Valid @RequestBody ProductDTO productDTO) {
             if(!loggedUser.canWrite("produto")){
             return new ResponseEntity<>(HttpStatus.FORBIDDEN);
             }
-            if (productDTO.getNome().isBlank() || productDTO.getPreco_sugerido() == null){
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-            }
-            if(productDTO.getNome().isBlank() == false){
-                service.isUniqueNomeProduct(productDTO);
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-            }
-            if (productDTO.getPreco_sugerido().compareTo(BigDecimal.ZERO) <= 0){
-                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-            }
-
             Long productId = service.insert(productDTO);
             return new ResponseEntity<>(productId, HttpStatus.CREATED);
-
     }
+
 
 }
