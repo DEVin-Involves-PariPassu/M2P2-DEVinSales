8bc900f62e3fa6d93dbc3e7d5f76c163793440f9
Daiana Michels <daianaem.12@gmail.com>
2022-03-07 13:52:19 -0300

    feat: Altera e simplifica métodos e validações para o Post

--- src/main/java/br/com/senai/p2m02/devinsales/service/ProductService.java
+++ src/main/java/br/com/senai/p2m02/devinsales/service/ProductService.java
@@ -3,11 +3,13 @@ package br.com.senai.p2m02.devinsales.service;
 import br.com.senai.p2m02.devinsales.dto.ProductDTO;
 import br.com.senai.p2m02.devinsales.model.ProductEntity;
 import br.com.senai.p2m02.devinsales.repository.ProductRepository;
+import br.com.senai.p2m02.devinsales.service.exception.RequiredFieldMissingException;
 import jakarta.persistence.EntityExistsException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.validation.Valid;
+
+import java.math.BigDecimal;
 import java.util.Optional;
 
 @Service
@@ -16,31 +18,48 @@ public class ProductService {
     @Autowired
     ProductRepository productRepository;
 
-
-    //para inserir um novo produto - usado no PostMapping
-    public Long insert(@Valid ProductDTO productDTO) {
-        ProductEntity product = new ProductEntity(productDTO.getId(),
-                productDTO.getNome(),
-                productDTO.getPreco_sugerido());
-        productRepository.products.add(product);
+    public Long insert(ProductDTO productDTO){
+        ProductEntity product = validationsPost(productDTO);
+        product = productRepository.save(product);
         return product.getId();
     }
 
-    //busca um produto pelo Id e retorna ele
-    public ProductEntity findById(Long productId) {
-        for (ProductEntity product : productRepository.products) {
-            if (product.getId() == productId)
-                return product;
-        }
-        return null;
+    private ProductEntity validationsPost(ProductDTO productDTO){
+        existsNome(productDTO);
+        isUniqueNomeProduct(productDTO);
+        existsPreco(productDTO);
+        precoValido(productDTO);
+
+        ProductEntity product = new ProductEntity();
+        product.setNome(productDTO.getNome());
+        product.setPreco_sugerido(productDTO.getPreco_sugerido());
+
+        return product;
     }
 
-    public void isUniqueNomeProduct(ProductDTO productDTO) {
+    private void precoValido(ProductDTO productDTO){
+        if(productDTO.getPreco_sugerido().compareTo(BigDecimal.ZERO) <= 0){
+            throw new RequiredFieldMissingException("Valor do produto inválido.");
+        }
+    }
+
+    private void existsPreco(ProductDTO productDTO){
+        if(productDTO.getPreco_sugerido() == null){
+            throw new RequiredFieldMissingException("O valor do produto é obrigatório.");
+        }
+    }
+
+    private void isUniqueNomeProduct(ProductDTO productDTO) {
         Optional<ProductEntity> optionalProduct = productRepository.findByNome(productDTO.getNome());
         if(optionalProduct.isPresent()){
             throw new EntityExistsException("Já existe um produto cadastrado com o nome " + productDTO.getNome());
         }
     }
 
+    private void existsNome(ProductDTO productDTO){
+        if(productDTO.getNome().isBlank()){
+            throw new RequiredFieldMissingException("O nome do produto é obrigatório.");
+        }
+    }
 
 }
