8f61758b250b0287826f491bc8c05d97062efeba
Victor Ferrando <victor.ferrando@hotmail.com>
2022-03-07 20:54:47 -0300

    Created UserEntityController.java

---
+++ src/main/java/br/com/senai/p2m02/devinsales/api/v1/UserEntityController.java
@@ -0,0 +1,12 @@
+package br.com.senai.p2m02.devinsales.api.v1;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+public class UserEntityController {
+
+
+
+
+}
