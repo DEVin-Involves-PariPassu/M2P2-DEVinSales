fc063680db09e204d5936de1db259bf8a6f05a24
Daiana Michels <daianaem.12@gmail.com>
2022-03-07 13:49:07 -0300

    feat: Deleta List<> products

--- src/main/java/br/com/senai/p2m02/devinsales/repository/ProductRepository.java
+++ src/main/java/br/com/senai/p2m02/devinsales/repository/ProductRepository.java
@@ -4,15 +4,11 @@ import br.com.senai.p2m02.devinsales.model.ProductEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
+
 import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends CrudRepository<ProductEntity, Long> {
-
-    List<ProductEntity> products = new ArrayList<>();
 
     Optional<ProductEntity> findByNome(String nome);
 }
