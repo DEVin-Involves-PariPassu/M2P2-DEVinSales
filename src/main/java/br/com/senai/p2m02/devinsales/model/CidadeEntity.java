cc942617598f76ddab5498756d8bb8084c5baa81
bruno-l1m2 <brunolyma@gmail.com>
2022-03-07 21:30:46 -0300

    Cria Endpoint: DELETE /state/{id_state}/city/{id_city}/address/{id_address}

--- src/main/java/br/com/senai/p2m02/devinsales/model/CidadeEntity.java
+++ src/main/java/br/com/senai/p2m02/devinsales/model/CidadeEntity.java
@@ -12,7 +12,7 @@ public class CidadeEntity {
 
     private String nome;
 
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne(cascade = {CascadeType.MERGE,CascadeType.PERSIST})
     @JoinColumn(name = "id_estado", referencedColumnName = "id")
     private EstadoEntity estado;
 
