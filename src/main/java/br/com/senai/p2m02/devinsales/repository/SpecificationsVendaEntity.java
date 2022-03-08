39c0970b7153b7cad86d897a925426531b910e85
Cleberson <silvacleberson36@gmail.com>
2022-03-08 11:07:36 -0300

    syntax error correction
    syntax error correction

--- src/main/java/br/com/senai/p2m02/devinsales/repository/SpecificationsVendaEntity.java
+++ src/main/java/br/com/senai/p2m02/devinsales/repository/SpecificationsVendaEntity.java
@@ -4,7 +4,7 @@ import br.com.senai.p2m02.devinsales.model.EstadoEntity;
 import br.com.senai.p2m02.devinsales.model.VendaEntity;
 import org.springframework.data.jpa.domain.Specification;
 
-public class git SpecificationsVendaEntity {
+public class SpecificationsVendaEntity {
     //WHERE vendedor = :idVendedor
     public static Specification<VendaEntity> idVendedor(Long idVendedor){
         return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("vendedor"), idVendedor);
