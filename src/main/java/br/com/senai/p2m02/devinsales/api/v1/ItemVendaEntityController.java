dce1812f32ee76e11eef73d15a8c84237797a531
LucasFL <lucas.f.lotaif@gmail.com>
2022-03-09 20:59:23 -0300

    Merge SalesController into ItemVendaEntityController

--- src/main/java/br/com/senai/p2m02/devinsales/api/v1/ItemVendaEntityController.java
+++ src/main/java/br/com/senai/p2m02/devinsales/api/v1/ItemVendaEntityController.java
@@ -1,22 +1,23 @@
 package br.com.senai.p2m02.devinsales.api.v1;
 
 import br.com.senai.p2m02.devinsales.model.UserEntity;
-
 import br.com.senai.p2m02.devinsales.service.ItemVendaEntityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+
 
 @RestController
-@RequestMapping("/sales/{id_venda}/item")
+@RequestMapping("/sales/{id_venda}/item/{id_item}/")
 public class ItemVendaEntityController {
 
     @Autowired
     private ItemVendaEntityService service;
 
-    @PatchMapping("/{id_item}/quantity/{quantity}")
+    @PatchMapping("/quantity/{quantity}")
     public ResponseEntity<Void> patchQuantidade(@PathVariable(name = "id_venda") Long idVenda,
                                                 @PathVariable(name = "id_item") Long idItem,
                                                 @PathVariable Integer quantity,
@@ -26,6 +27,22 @@ public class ItemVendaEntityController {
         }
         service.patchQuantity(idVenda, idItem, quantity);
         return ResponseEntity.noContent().build();
+
+    }
+
+    @PatchMapping("/price/{price}")
+    public ResponseEntity<Object> updateItemVendaPrice(
+            @PathVariable("id_venda") Long idVenda,
+            @PathVariable("id_item") Long idItem,
+            @PathVariable("price") BigDecimal price,
+            @RequestAttribute("loggedUser") UserEntity loggedUser
+    ) {
+
+        if (!loggedUser.canWrite("venda")) {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+
+        return service.patchPrice(idVenda, idItem, price);
 
     }
 }
