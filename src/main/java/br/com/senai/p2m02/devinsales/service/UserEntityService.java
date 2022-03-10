a4e392cc6d2e5f75f58fa718bae208b2a4bc243b
Kalyana Greim <kalygreim11@gmail.com>
2022-03-10 15:27:37 -0300

    Update UserEntityService.java
    Alterando nome da classe para evitar conflitos de duplicação de classe com mesmo objetivo

--- src/main/java/br/com/senai/p2m02/devinsales/service/UserEntityService.java
+++ src/main/java/br/com/senai/p2m02/devinsales/service/UserEntityService.java
@@ -11,7 +11,7 @@ import java.time.LocalDate;
 import java.util.Optional;
 
 @Service
-public class UserService {
+public class UserEntityService {
 
     @Autowired
     UserEntityRepository userRepository;
