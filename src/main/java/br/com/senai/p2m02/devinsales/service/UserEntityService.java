4866a3dd16e58d6a1dacb639e4ca1639353c4f20
Kalyana Greim <kalygreim11@gmail.com>
2022-03-11 20:14:59 -0300

    Validações feature
    cria validações da feature

--- src/main/java/br/com/senai/p2m02/devinsales/service/UserEntityService.java
+++ src/main/java/br/com/senai/p2m02/devinsales/service/UserEntityService.java
@@ -1,5 +1,6 @@
 package br.com.senai.p2m02.devinsales.service;
 
+import br.com.senai.p2m02.devinsales.dto.FeatureDTO;
 import br.com.senai.p2m02.devinsales.model.FeatureEntity;
 import br.com.senai.p2m02.devinsales.model.UserEntity;
 import br.com.senai.p2m02.devinsales.model.UserFeatureEntity;
@@ -7,17 +8,19 @@ import br.com.senai.p2m02.devinsales.model.UserFeatureId;
 import br.com.senai.p2m02.devinsales.repository.FeatureEntityRepository;
 import br.com.senai.p2m02.devinsales.repository.UserEntityRepository;
 import br.com.senai.p2m02.devinsales.repository.UserFeatureEntityRepository;
+import br.com.senai.p2m02.devinsales.service.exception.RequiredFieldMissingException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import br.com.senai.p2m02.devinsales.dto.UserDTO;
 import br.com.senai.p2m02.devinsales.service.exception.UserIsUnderAgeException;
 import jakarta.persistence.EntityExistsException;
+
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class UserEntityService {
@@ -43,7 +46,7 @@ public class UserEntityService {
         Optional<UserFeatureEntity> optionalUserFeature = userFeatureEntityRepository.findById(idUserFeature);
         UserFeatureEntity userFeatureEntity;
 
-        if(optionalUserFeature.isEmpty()) {
+        if (optionalUserFeature.isEmpty()) {
             userFeatureEntity = new UserFeatureEntity();
             userFeatureEntity.setId(new UserFeatureId(userEntity.getId(), featureEntity.getId()));
             userFeatureEntity.setUser(userEntity);
@@ -53,7 +56,7 @@ public class UserEntityService {
         } else {
             userFeatureEntity = optionalUserFeature.get();
         }
-        if(tipoPermissao.equals("read")) {
+        if (tipoPermissao.equals("read")) {
             userFeatureEntity.setRead(true);
             userFeatureEntity.setWrite(false);
         } else if (tipoPermissao.equals("write")) {
@@ -68,26 +71,26 @@ public class UserEntityService {
         userFeatureEntityRepository.save(userFeatureEntity);
     }
 
-    public Long salvar(UserDTO user){
-        UserEntity newUser = validationsUser(user);
+    public Long salvar(UserDTO user) {
+        UserEntity newUser = validateUser(user);
         userEntityRepository.save(newUser);
         return newUser.getId();
     }
 
-    public void atualizar(Long idUser, UserDTO userDTO){
+    public void atualizar(Long idUser, UserDTO userDTO) {
         UserEntity user = updateUser(idUser, userDTO);
 
         userEntityRepository.save(user);
     }
 
-    public UserEntity updateUser(Long idUser, UserDTO userDTO){
+    public UserEntity updateUser(Long idUser, UserDTO userDTO) {
         isUniqueNameUser(userDTO);
         isUniqueLoginUser(userDTO);
         LocalDate dtNascimento = verificationDate(userDTO);
         verificationAge(dtNascimento);
 
         UserEntity user = userEntityRepository.findById(idUser).orElseThrow(
-                ()-> new EntityNotFoundException("Id de usuário inexistente.")
+                () -> new EntityNotFoundException("Id de usuário inexistente.")
         );
         user.setLogin(userDTO.getLogin());
         user.setSenha(userDTO.getSenha());
@@ -97,26 +100,54 @@ public class UserEntityService {
         return user;
     }
 
-    private UserEntity validationsUser(UserDTO user) {
+    private UserEntity validateUser(UserDTO user) {
 
+        existsNome(user);
+        existsLogin(user);
+        existsSenha(user);
+        existsDtNascimento(user);
         isUniqueNameUser(user);
         isUniqueLoginUser(user);
         LocalDate userAge = verificationDate(user);
         verificationAge(userAge);
+        isFeaturesEmpty(user);
 
         UserEntity newUser = new UserEntity();
         newUser.setLogin(user.getLogin());
         newUser.setSenha(user.getSenha());
         newUser.setNome(user.getNome());
         newUser.setDtNascimento(userAge);
+        newUser = userEntityRepository.save(newUser);
+        Set<UserFeatureEntity> userFeatureEntities = validateFeatures(newUser, user);
+        newUser.setUserFeatureEntities(userFeatureEntities);
 
         return newUser;
     }
 
+    private void existsNome(UserDTO user) {
+        if (user.getNome() == null){
+            throw new RequiredFieldMissingException("O campo Nome é obrigatório");
+        }
+    }
+    private void existsLogin(UserDTO user) {
+        if (user.getLogin() == null){
+            throw new RequiredFieldMissingException("O campo Login é obrigatório");
+        }
+    }
+    private void existsSenha(UserDTO user) {
+        if (user.getSenha() == null){
+            throw new RequiredFieldMissingException("O campo Senha é obrigatório");
+        }
+    }
+    private void existsDtNascimento(UserDTO user) {
+        if (user.getDtNascimento() == null){
+            throw new RequiredFieldMissingException("O campo dtNascimento é obrigatório");
+        }
+    }
     private void verificationAge(LocalDate dtNascimento) {
         Period idade = Period.between(dtNascimento, LocalDate.now());
 
-        if(idade.getYears() < 18) {
+        if (idade.getYears() < 18) {
             throw new UserIsUnderAgeException("O usuário deve possuir mais de 18 anos.");
         }
     }
@@ -131,17 +162,56 @@ public class UserEntityService {
 
     private void isUniqueLoginUser(UserDTO user) {
         Optional<UserEntity> optionalUser = userEntityRepository.findUserEntityByLogin(user.getLogin());
-        if(optionalUser.isPresent()){
+        if (optionalUser.isPresent()) {
             throw new EntityExistsException("Já existe um usuário cadastrado com este login: " + user.getLogin());
         }
     }
 
     private void isUniqueNameUser(UserDTO user) {
         Optional<UserEntity> optionalUser = userEntityRepository.findUserEntityByNome(user.getNome());
-        if(optionalUser.isPresent()){
+        if (optionalUser.isPresent()) {
             throw new EntityExistsException("Já existe este nome de usuário cadastrado: " + user.getNome());
         }
     }
 
-    private void validationFeature(UserEntity user) { }
+    private void isFeaturesEmpty(UserDTO user) {
+
+        if (user.getFeatures() == null || user.getFeatures().isEmpty()) {
+            throw new IllegalArgumentException("A lista de features não existe ou é vazia");
+        }
+    }
+
+    private Set<UserFeatureEntity> validateFeatures(UserEntity userEntity, UserDTO user) {
+        Set<UserFeatureEntity> userFeatureEntities = new HashSet<>();
+        for (FeatureDTO feature : user.getFeatures()) {
+            FeatureEntity featureEntity = existsFeature(feature);
+            UserFeatureEntity userFeatureEntity = new UserFeatureEntity();
+            userFeatureEntity.setId(new UserFeatureId(userEntity.getId(), featureEntity.getId()));
+            userFeatureEntity.setUser(userEntity);
+            userFeatureEntity.setFeature(featureEntity);
+            if (feature.getRead() == null) {
+                userFeatureEntity.setRead(false);
+            } else {
+                userFeatureEntity.setRead(feature.getRead());
+            }
+            if (feature.getWrite() == null) {
+                userFeatureEntity.setWrite(false);
+            } else {
+                userFeatureEntity.setWrite(feature.getWrite());
+            }
+            userFeatureEntity = userFeatureEntityRepository.save(userFeatureEntity);
+            userFeatureEntities.add(userFeatureEntity);
+
+        }
+        return userFeatureEntities;
+    }
+
+    private FeatureEntity existsFeature(FeatureDTO feature) {
+        FeatureEntity featureEntity = featureEntityRepository.findFirstByNomeFeature(feature.getFeature()).orElseThrow(
+                () -> new IllegalArgumentException("Não existe feature com o nome " + feature.getFeature())
+        );
+        return featureEntity;
+    }
+
+
 }
