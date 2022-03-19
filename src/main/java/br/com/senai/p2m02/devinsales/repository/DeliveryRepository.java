acd834061239bb12c699dfad57e3cfaeab65caec
LucasFL <lucas.f.lotaif@gmail.com>
2022-03-19 13:17:00 -0300

    Adds repository annotation

--- src/main/java/br/com/senai/p2m02/devinsales/repository/DeliveryRepository.java
+++ src/main/java/br/com/senai/p2m02/devinsales/repository/DeliveryRepository.java
@@ -4,9 +4,9 @@ package br.com.senai.p2m02.devinsales.repository;
 import br.com.senai.p2m02.devinsales.model.DeliveryEntity;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-
-
+@Repository
 public interface DeliveryRepository extends CrudRepository<DeliveryEntity, Long> ,
         JpaSpecificationExecutor<DeliveryEntity>{
 
