3fbaafed83d8fbcd413a43f9febb85d7abb07dd0
Lucas Bueno Cesario <lbc92@hotmail.com>
2022-04-23 21:54:50 -0300

    Adiciona testes do EstadoEntityService: listar e salvar

--- src/test/java/br/com/senai/p2m02/devinsales/service/EstadoServiceTests.java
+++ src/test/java/br/com/senai/p2m02/devinsales/service/EstadoServiceTests.java
@@ -1,12 +1,12 @@
 package br.com.senai.p2m02.devinsales.service;
 
 
+import br.com.senai.p2m02.devinsales.dto.EstadoDTO;
 import br.com.senai.p2m02.devinsales.model.EstadoEntity;
-import br.com.senai.p2m02.devinsales.model.ProductEntity;
 import br.com.senai.p2m02.devinsales.model.SiglaEstado;
+import br.com.senai.p2m02.devinsales.repository.CidadeEntityRepository;
 import br.com.senai.p2m02.devinsales.repository.EstadoEntityRepository;
 import br.com.senai.p2m02.devinsales.repository.SpecificationsEstadoEntity;
-import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -14,6 +14,11 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.util.List;
@@ -23,13 +28,16 @@ import static org.mockito.Mockito.*;
 
 public class EstadoServiceTests {
     @Mock
-    private EstadoEntityRepository repository;
+    private EstadoEntityRepository estadoEntityRepository;
+
+    @Mock
+    private CidadeEntityRepository cidadeEntityRepository;
 
 //    @Mock
 //    private SpecificationsEstadoEntity specifications;
 
     @InjectMocks
-    private EstadoEntityService service = new EstadoEntityService();
+    private EstadoEntityService service;
 
     @BeforeEach
     public void setup(){
@@ -45,16 +53,43 @@ public class EstadoServiceTests {
         estadoEntity.setNome("Acre");
         estadoEntity.setSigla(SiglaEstado.AC);
 
-        Specification specification = Specification.where(
-                SpecificationsEstadoEntity.nome(estadoEntity.getNome())
-        );
-
-        when(repository.findAll(specification)).thenReturn(List.of(estadoEntity));
+        when(estadoEntityRepository.findAll(any(Specification.class))).thenReturn(List.of(estadoEntity));
         // Execução
         List<EstadoEntity> estadosRetornados = service.listar(estadoEntity.getNome());
         // Validação
         Assertions.assertEquals(List.of(estadoEntity), estadosRetornados);
-        //verify(this.repository, times(1)).findAll(specification);
+        verify(this.estadoEntityRepository, times(1)).findAll(any(Specification.class));
+    }
+
+    @Test
+    @DisplayName("Salvar Estado")
+    public void deveSalvarEstado(){
+        //Cenário
+//        EstadoEntity estadoEntity = new EstadoEntity();
+//        estadoEntity.setNome("Distrito Federal");
+//        estadoEntity.setSigla(SiglaEstado.DF);
+
+        EstadoDTO estadoDTO = new EstadoDTO();
+        estadoDTO.setNome("Distrito Federal");
+        estadoDTO.setSigla("DF");
+
+        when(estadoEntityRepository.findFirstByNome(estadoDTO.getNome())).thenReturn(Optional.empty());
+        when(estadoEntityRepository.findFirstBySigla(SiglaEstado.valueOf(estadoDTO.getSigla()))).thenReturn(Optional.empty());
+        when(estadoEntityRepository.save(any(EstadoEntity.class))).thenAnswer(new Answer<EstadoEntity>() {
+            public EstadoEntity answer(InvocationOnMock invocation) throws Throwable {
+                EstadoEntity estadoRetornado = invocation.getArgument(0);
+                if (estadoRetornado != null) {
+                    estadoRetornado.setId(27L);
+                }
+                return estadoRetornado;
+            }
+        });
+
+        // Execução
+        Long idEstado = service.salvar(estadoDTO);
+        // Validação
+        Assertions.assertEquals(27L, idEstado);
+        verify(this.estadoEntityRepository, times(1)).save(any(EstadoEntity.class));
     }
 }
 
