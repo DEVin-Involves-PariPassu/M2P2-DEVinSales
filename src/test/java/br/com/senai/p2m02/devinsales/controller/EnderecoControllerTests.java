94d9ac5a2673d6d522e8487a50563d235a68dad7
felixcolombo <89814084+felixcolombo@users.noreply.github.com>
2022-04-28 19:19:14 -0300

    Teste - Listar Endereços Sem Autorização

--- src/test/java/br/com/senai/p2m02/devinsales/controller/EnderecoControllerTests.java
+++ src/test/java/br/com/senai/p2m02/devinsales/controller/EnderecoControllerTests.java
@@ -189,4 +189,51 @@ public class EnderecoControllerTests {
                 .andExpect(status().isBadRequest());
     }
 
+    @Test
+    @DisplayName("Listar Endereços Sem Autorização")
+    public void naoDeveListarEnderecosQuandoNaoForAutorizado() throws Exception {
+        // gerando o token
+        String body = "{\"login\":\"camilla\",\"senha\":\"camilla123\"}";
+
+        MvcResult result = mockMvc
+                .perform(MockMvcRequestBuilders.post("/auth")
+                        .header("Content-Type", "application/json" )
+                        .content(body))
+                .andExpect(status().isOk()).andReturn();
+
+        // extraindo o token
+        String response = result.getResponse().getContentAsString();
+        JSONObject json = new JSONObject(response);
+        String token = (String) json.get("token");
+
+        Assertions.assertNotNull(token);
+
+        EnderecoEntity endereco = new EnderecoEntity();
+        CidadeEntity cidade = new CidadeEntity();
+        EstadoEntity estado = new EstadoEntity();
+        cidade.setId(1L);
+        cidade.setNome("Florianopolis");
+        cidade.setEstado(estado);
+        estado.setId(1L);
+        estado.setNome("Santa Catarina");
+        estado.setSigla(SiglaEstado.SC);
+        endereco.setId(1L);
+        endereco.setRua("Rua Principal");
+        endereco.setNumero(123);
+        endereco.setCidade(cidade);
+        endereco.setComplemento("Primavera Garden");
+
+        when(enderecoEntityService.listarPorId (
+                1L,
+                1L,
+                1L)).thenReturn(endereco);
+
+        //executando controller com o token
+        mockMvc.perform(MockMvcRequestBuilders.get("/state/{id_state}/city/{id_city}/address/{id_address}",1L, 1L, 1l)
+                        .header("Authorization", "Bearer " + token)
+                        .header("Content-Type", "application/json" )
+                )
+                .andExpect(status().isForbidden());
+    }
+
 }
