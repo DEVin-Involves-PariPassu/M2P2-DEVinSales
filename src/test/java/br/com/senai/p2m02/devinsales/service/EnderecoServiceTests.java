13a4cf095b86d9c159ee24443a8127c6e809497c
felixcolombo <89814084+felixcolombo@users.noreply.github.com>
2022-04-28 20:22:32 -0300

    EnderecoServiceTests - Teste Salvar Endereço com Corpo da Requisição Completo

---
+++ src/test/java/br/com/senai/p2m02/devinsales/service/EnderecoServiceTests.java
@@ -0,0 +1,88 @@
+package br.com.senai.p2m02.devinsales.service;
+
+import br.com.senai.p2m02.devinsales.dto.CidadeDTO;
+import br.com.senai.p2m02.devinsales.dto.EnderecoDTO;
+import br.com.senai.p2m02.devinsales.model.CidadeEntity;
+import br.com.senai.p2m02.devinsales.model.EnderecoEntity;
+import br.com.senai.p2m02.devinsales.model.EstadoEntity;
+import br.com.senai.p2m02.devinsales.model.SiglaEstado;
+import br.com.senai.p2m02.devinsales.repository.CidadeEntityRepository;
+import br.com.senai.p2m02.devinsales.repository.EnderecoEntityRepository;
+import br.com.senai.p2m02.devinsales.repository.EstadoEntityRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+public class EnderecoServiceTests {
+
+    @Mock
+    private EstadoEntityRepository estadoEntityRepository;
+
+    @Mock
+    private CidadeEntityRepository cidadeEntityRepository;
+
+    @Mock
+    private EnderecoEntityRepository enderecoEntityRepository;
+
+    @InjectMocks
+    private EnderecoEntityService enderecoEntityService;
+
+    @BeforeEach
+    public void setup(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    @DisplayName("Salvar Endereço com Corpo da Requisição Completo")
+    public void deveSalvarEnderecoQuandoCorpoEstaCompleto(){
+
+        EstadoEntity estado = new EstadoEntity();
+        CidadeEntity cidade = new CidadeEntity();
+
+        cidade.setId(1L);
+        cidade.setNome("Florianópolis");
+        cidade.setEstado(estado);
+
+        estado.setId(1L);
+        estado.setNome("Santa Catarina");
+        estado.setSigla(SiglaEstado.SC);
+
+        EnderecoDTO enderecoDTO = new EnderecoDTO();
+        enderecoDTO.setEstadoId(1L);
+        enderecoDTO.setCidadeId(1L);
+        enderecoDTO.setRua("Rua Principal");
+        enderecoDTO.setNumero(123);
+        enderecoDTO.setComplemento("DevInHouse");
+
+        when(estadoEntityRepository.findById(estado.getId())).thenReturn(Optional.of(estado));
+        when(cidadeEntityRepository.findById(cidade.getId())).thenReturn(Optional.of(cidade));
+        when(enderecoEntityRepository.save(any(EnderecoEntity.class))).thenAnswer(new Answer<EnderecoEntity>() {
+            public EnderecoEntity answer(InvocationOnMock invocation) throws Throwable {
+                EnderecoEntity enderecoRetornado = invocation.getArgument(0);
+                if (enderecoRetornado != null) {
+                    enderecoRetornado.setId(1L);
+                }
+                return enderecoRetornado;
+            }
+        });
+
+        // Execução
+        Long idEndereco = enderecoEntityService.salvar(enderecoDTO, estado.getId(), cidade.getId());
+        // Validação
+        Assertions.assertEquals(1L, idEndereco);
+        verify(this.estadoEntityRepository, times(1)).findById(estado.getId());
+        verify(this.cidadeEntityRepository, times(1)).findById(cidade.getId());
+        verify(this.enderecoEntityRepository, times(1)).save(any(EnderecoEntity.class));
+    }
+}
