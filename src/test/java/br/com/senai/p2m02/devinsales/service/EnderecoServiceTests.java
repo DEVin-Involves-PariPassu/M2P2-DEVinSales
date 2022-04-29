4bd6c8c3836723ee25f6a45575e972ab381e12ca
felixcolombo <89814084+felixcolombo@users.noreply.github.com>
2022-04-28 21:02:03 -0300

    EnderecoServiceTests - Teste Não Salvar Endereço Quando Rua for Nulo

--- src/test/java/br/com/senai/p2m02/devinsales/service/EnderecoServiceTests.java
+++ src/test/java/br/com/senai/p2m02/devinsales/service/EnderecoServiceTests.java
@@ -1,5 +1,6 @@
 package br.com.senai.p2m02.devinsales.service;
 
+import br.com.senai.p2m02.devinsales.dto.CidadeDTO;
 import br.com.senai.p2m02.devinsales.dto.EnderecoDTO;
 import br.com.senai.p2m02.devinsales.model.CidadeEntity;
 import br.com.senai.p2m02.devinsales.model.EnderecoEntity;
@@ -8,6 +9,7 @@ import br.com.senai.p2m02.devinsales.model.SiglaEstado;
 import br.com.senai.p2m02.devinsales.repository.CidadeEntityRepository;
 import br.com.senai.p2m02.devinsales.repository.EnderecoEntityRepository;
 import br.com.senai.p2m02.devinsales.repository.EstadoEntityRepository;
+import br.com.senai.p2m02.devinsales.service.exception.RequiredFieldMissingException;
 import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -148,6 +150,38 @@ public class EnderecoServiceTests {
         });
         // Validação
         verify(this.cidadeEntityRepository, times(1)).findById(2L);
+    }
+
+    @Test
+    @DisplayName("Não Salvar Endereço Quando Rua for Nulo")
+    public void naoDeveSalvarEnderecoQuandoRuaForNulo(){
+
+        EstadoEntity estado = new EstadoEntity();
+        estado.setId(1L);
+        estado.setNome("Santa Cataria");
+        estado.setSigla(SiglaEstado.SC);
+
+        CidadeEntity cidade = new CidadeEntity();
+        cidade.setId(1L);
+        cidade.setEstado(estado);
+        cidade.setNome("Florianópolis");
+
+        EnderecoDTO enderecoDTO = new EnderecoDTO();
+        enderecoDTO.setEstadoId(1L);
+        enderecoDTO.setNumero(123);
+        enderecoDTO.setCidadeId(1L);
+        enderecoDTO.setComplemento("DevInHouse");
+
+        when(estadoEntityRepository.findById(estado.getId())).thenReturn(Optional.of(estado));
+        when(cidadeEntityRepository.findById(cidade.getId())).thenReturn(Optional.of(cidade));
+
+        // Execução
+        Assertions.assertThrows(RequiredFieldMissingException.class, ()-> {
+            Long idEndereco = enderecoEntityService.salvar(enderecoDTO, 1L, 1L);
+        });
+        // Validação
+        verify(this.estadoEntityRepository, times(1)).findById(estado.getId());
+        verify(this.cidadeEntityRepository, times(1)).findById(cidade.getId());
     }
 
 }
