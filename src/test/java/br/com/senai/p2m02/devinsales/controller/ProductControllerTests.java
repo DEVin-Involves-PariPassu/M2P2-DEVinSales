51fe3135d6958f648888c2baecb239a9ecacdf07
Daiana Michels <daianaem.12@gmail.com>
2022-04-30 10:50:47 -0300

    feat: adiciona tests classe ProductController

--- src/test/java/br/com/senai/p2m02/devinsales/controller/ProductControllerTests.java
+++ src/test/java/br/com/senai/p2m02/devinsales/controller/ProductControllerTests.java
@@ -3,8 +3,10 @@ package br.com.senai.p2m02.devinsales.controller;
 import br.com.senai.p2m02.devinsales.dto.ProductDTO;
 import br.com.senai.p2m02.devinsales.model.ProductEntity;
 import br.com.senai.p2m02.devinsales.service.ProductService;
+import br.com.senai.p2m02.devinsales.service.exception.RequiredFieldMissingException;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -17,6 +19,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import java.math.BigDecimal;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -24,34 +27,30 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class ProductControllerTests {
 
+
     @Autowired
-    MockMvc mockMvc;
+    private MockMvc mockMvc;
 
     @MockBean
-    ProductService service;
+    private ProductService service;
+
 
     @Test
-    public void deveCriarUmProdutoQuandoForAutenticado() throws Exception {
-        // gerando o token
+    @DisplayName("Criar novo produto")
+    public void deveCriarUmProdutoQuandoEstiverAutenticado() throws Exception {
         String body = "{\"login\":\"admin\",\"senha\":\"admin123\"}";
+
         MvcResult result = mockMvc
                 .perform(MockMvcRequestBuilders.post("/auth")
                         .header("Content-Type", "application/json" )
                         .content(body))
                 .andExpect(status().isOk()).andReturn();
 
-
-        // extraindo o token
         String response = result.getResponse().getContentAsString();
         JSONObject json = new JSONObject(response);
         String token = (String) json.get("token");
 
         Assertions.assertNotNull(token);
-
-        ProductEntity produto = new ProductEntity();
-        produto.setNome("batata");
-        produto.setPreco_sugerido(BigDecimal.valueOf(0.50));
-        produto.setId(1L);
 
         when(service.insert(any(ProductDTO.class))).thenReturn(1L);
 
@@ -60,7 +59,6 @@ public class ProductControllerTests {
                 "    \"preco_sugerido\":0.50\n" +
                 "}";
 
-        //executando controller com o token
         MvcResult resultPost = mockMvc.perform(MockMvcRequestBuilders.post("/product")
                         .header("Authorization", "Bearer " + token)
                         .header("Content-Type", "application/json" )
@@ -73,4 +71,182 @@ public class ProductControllerTests {
         Assertions.assertEquals
                 ("1", responsePost);
     }
-}
+
+    @Test
+    @DisplayName("Nome produto incorreto - Bad Request")
+    public void deveRetornarBadRequestQuandoInformarUmProdutoComNomeInvalido() throws Exception {
+        String body = "{\"login\":\"admin\",\"senha\":\"admin123\"}";
+
+        MvcResult result = mockMvc
+                .perform(MockMvcRequestBuilders.post("/auth")
+                        .header("Content-Type", "application/json" )
+                        .content(body))
+                .andExpect(status().isOk()).andReturn();
+
+        String response = result.getResponse().getContentAsString();
+        JSONObject json = new JSONObject(response);
+        String token = (String) json.get("token");
+
+        Assertions.assertNotNull(token);
+
+
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setNome("");
+        productDTO.setPreco_sugerido(BigDecimal.valueOf(50.00));
+        when(service.insert(any(ProductDTO.class))).thenThrow(new RequiredFieldMissingException("O nome do produto é obrigatório."));
+
+        String bodyRequisicao = "{\n" +
+                "    \"preco_sugerido\":50.00\n" +
+                "}";
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/product")
+                        .header("Authorization", "Bearer " + token)
+                        .header("Content-Type", "application/json" )
+                        .content(bodyRequisicao)
+                )
+                .andExpect(status().isBadRequest());
+
+    }
+
+    @Test
+    @DisplayName("Preço do produto incorreto - Bad Request")
+    public void deveRetornarBadRequestQuandoInformarUmProdutoComPrecoInvalido() throws Exception {
+        String body = "{\"login\":\"admin\",\"senha\":\"admin123\"}";
+
+        MvcResult result = mockMvc
+                .perform(MockMvcRequestBuilders.post("/auth")
+                        .header("Content-Type", "application/json" )
+                        .content(body))
+                .andExpect(status().isOk()).andReturn();
+
+        String response = result.getResponse().getContentAsString();
+        JSONObject json = new JSONObject(response);
+        String token = (String) json.get("token");
+
+        Assertions.assertNotNull(token);
+
+
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setNome("Coffee");
+        when(service.insert(any(ProductDTO.class))).thenThrow(new RequiredFieldMissingException("O nome do produto é obrigatório."));
+
+        String bodyRequisicao = "{\n" +
+                "    \"nome\":\"Coffee\",\n" +
+                "}";
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/product")
+                        .header("Authorization", "Bearer " + token)
+                        .header("Content-Type", "application/json" )
+                        .content(bodyRequisicao)
+                )
+                .andExpect(status().isBadRequest());
+
+    }
+
+
+    @Test
+    @DisplayName("Post/Product sem autenticacao")
+    public void naoDeveAdicionarUmNovoProdutoQuandoNaoForAutenticado() throws Exception{
+            String body = "{\"login\":\"naoexiste\",\"senha\":\"naoexiste123\"}";
+
+            mockMvc
+                    .perform(MockMvcRequestBuilders.post("/product")
+                            .header("Content-Type", "application/json" )
+                            .content(body))
+                    .andExpect(status().isForbidden());
+        }
+
+    @Test
+    @DisplayName("Atualizar produto")
+    public void deveAtualizarOProdutoQuandoEstiverAutenticado() throws Exception {
+        String body = "{\"login\":\"admin\",\"senha\":\"admin123\"}";
+        MvcResult result = mockMvc
+                .perform(MockMvcRequestBuilders.post("/auth")
+                        .header("Content-Type", "application/json" )
+                        .content(body))
+                .andExpect(status().isOk()).andReturn();
+
+        String response = result.getResponse().getContentAsString();
+        JSONObject json = new JSONObject(response);
+        String token = (String) json.get("token");
+
+        Assertions.assertNotNull(token);
+
+        ProductEntity produto = new ProductEntity();
+        produto.setNome("batata");
+        produto.setPreco_sugerido(BigDecimal.valueOf(0.50));
+        produto.setId(1L);
+
+        when(service.updateDoPut(anyLong(), any(ProductDTO.class))).thenReturn(1L);
+
+        String bodyRequisicao = "{\n" +
+                "    \"nome\":\"batatas\",\n" +
+                "    \"preco_sugerido\":0.55\n" +
+                "}";
+
+        //executando controller com o token
+        MvcResult resultPut = mockMvc.perform(MockMvcRequestBuilders.put("/product/{id_produto}", 1L)
+                        .header("Authorization", "Bearer " + token)
+                        .header("Content-Type", "application/json" )
+                        .content(bodyRequisicao)
+                )
+                .andExpect(status().isNoContent())
+                .andReturn();
+        String responsePut = resultPut.getResponse().getContentAsString();
+        Assertions.assertNotEquals(responsePut, "{\\n\" +\n" +
+                "                \"    \\\"nome\\\":\\\"batata\\\",\\n\" +\n" +
+                "                \"    \\\"preco_sugerido\\\":0.50\\n\" +\n" +
+                "                \"}");
+    }
+
+    @Test
+    @DisplayName("Produto inexistente - nulo")
+    public void deveRetornarBadRequestQuandoOIdDoProdutoForNulo() throws Exception {
+        String body = "{\"login\":\"admin\",\"senha\":\"admin123\"}";
+        MvcResult result = mockMvc
+                .perform(MockMvcRequestBuilders.post("/auth")
+                        .header("Content-Type", "application/json" )
+                        .content(body))
+                .andExpect(status().isOk()).andReturn();
+
+
+        String response = result.getResponse().getContentAsString();
+        JSONObject json = new JSONObject(response);
+        String token = (String) json.get("token");
+
+        Assertions.assertNotNull(token);
+
+
+        ProductEntity produto = new ProductEntity();
+        produto.setNome("batata");
+        produto.setPreco_sugerido(BigDecimal.valueOf(0.50));
+        produto.setId(1L);
+
+        when(service.updateDoPut(anyLong(), any(ProductDTO.class))).thenReturn(1L);
+
+        String bodyRequisicao = "{\n" +
+                "    \"nome\":\"CAFÉ\",\n" +
+                "    \"preco_sugerido\":55.50\n" +
+                "}";
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/product/{id_produto}", "null")
+                        .header("Authorization", "Bearer " + token)
+                        .header("Content-Type", "application/json" )
+                        .content(bodyRequisicao)
+                )
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("Put/Product sem autenticacao")
+    public void naoDeveAtualizarUmProdutoQuandoNaoForAutenticado() throws Exception{
+        String body = "{\"login\":\"naoexiste\",\"senha\":\"naoexiste123\"}";
+
+        mockMvc
+                .perform(MockMvcRequestBuilders.put("/product/1")
+                        .header("Content-Type", "application/json" )
+                        .content(body))
+                .andExpect(status().isForbidden());
+    }
+    }
+
